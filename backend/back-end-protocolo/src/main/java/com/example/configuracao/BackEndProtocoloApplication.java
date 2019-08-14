2911bef90b844bb6b0f1c5f8aaed53fbdbc1989d
igor036 <igorjoaquim.pg@gmail.com>
2019-08-14 19:57:25 -0300

    configurando banco

---
+++ backend/back-end-protocolo/src/main/java/com/example/configuracao/BackEndProtocoloApplication.java
@@ -0,0 +1,13 @@
+package com.example.configuracao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndProtocoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndProtocoloApplication.class, args);
+	}
+
+}
---
+++ backend/back-end-protocolo/src/main/java/com/example/configuracao/DBConfig.java
@@ -0,0 +1,22 @@
+package com.example.configuracao;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DBConfig {
+
+	@Bean
+	public DataSource mysqlDataSource(Environment env) {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.username"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
+		return dataSource;
+	}
+}
