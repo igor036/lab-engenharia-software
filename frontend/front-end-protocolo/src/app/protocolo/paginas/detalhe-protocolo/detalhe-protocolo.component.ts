e4321b54896b8f2f1b7ec4464acb03043b0ab884
igor036 <igorjoaquim.pg@gmail.com>
2019-10-21 22:02:44 -0300

    criando componente de detalhe de protocolo

---
+++ frontend/front-end-protocolo/src/app/protocolo/paginas/detalhe-protocolo/detalhe-protocolo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detalhe-protocolo',
+  templateUrl: './detalhe-protocolo.component.html',
+  styleUrls: ['./detalhe-protocolo.component.scss']
+})
+export class DetalheProtocoloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
