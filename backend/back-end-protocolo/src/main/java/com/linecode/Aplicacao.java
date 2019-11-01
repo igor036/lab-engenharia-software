83583674e791f57cf92c0031c4bab3078dd2da1a
pablo-vieira-59 <pablo.vieira.andrade@gmail.com>
2019-11-01 17:58:05 -0300

    Adicionado dependencias tomcat

--- backend/back-end-protocolo/src/main/java/com/linecode/Aplicacao.java
+++ backend/back-end-protocolo/src/main/java/com/linecode/Aplicacao.java
@@ -6,9 +6,10 @@ package com.linecode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Aplicacao {
+public class Aplicacao extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Aplicacao.class, args);
